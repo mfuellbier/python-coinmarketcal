aba252378dc0f78f69c42023e0eca79a6fbfc8e0
Greg NCube <gregoryberzin@gmail.com>
2018-11-27 14:00:52 +0200

    import fix

--- coinmarketcal/client.py
+++ coinmarketcal/client.py
@@ -14,6 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with python-coinmarketcal.  If not, see <http://www.gnu.org/licenses/>.
 
+import logging
 import json
 import requests
 from datetime import datetime
